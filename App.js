6887e3dfce65d7654ee5a47f8711720c78c816b1
Frederick-dev23 <f.p.n.97230@gmail.com>
2021-06-25 08:13:48 -0400

    appFood

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Mon text</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
